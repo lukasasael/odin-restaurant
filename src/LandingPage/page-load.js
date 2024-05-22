fe4f4dea857a3684262fa4b3a7eb50fb693b9dfe
Lukas Asael <lukasasaelbacelar@gmail.com>
2024-05-22 20:10:02 -0300

    Project Backbone
    Added the tabs, created simple HTML to landing through JS

---
+++ src/LandingPage/page-load.js
@@ -0,0 +1,14 @@
+export function pageLoad() {
+    const mainPage = document.getElementById("content")
+    const img = document.createElement("img")
+    img.src = "../../sources/LOGO.png"
+    mainPage.appendChild(img);
+
+    const heading = document.createElement("h1")
+    heading.textContent = "Fancy Indian Restaurant"
+    mainPage.appendChild(heading);
+
+    const paragraph = document.createElement("p")
+    paragraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    mainPage.appendChild(paragraph);
+}
